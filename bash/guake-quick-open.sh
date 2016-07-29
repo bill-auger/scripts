c441f6fb3db2e72723afc1702c796fbd010d1ed7
bill-auger <mr.j.spam.me@gmail.com>
2016-07-28 22:32:04 -0400

    add bash scripts

---
+++ bash/guake-quick-open.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# add to guake prefs:
+#     guake-quick-open "%(file_path)s" %(line_number)s
+
+FILE=$1
+LINE=$2
+ORIG_JUCE_BUILD_DIR=./Builds/LinuxMakefile
+NEW_JUCE_BUILD_DIR=./Builds/Linux
+MY_JUCE_BUILD_DIR=./Builds/Makefile
+
+
+[ -f "$ORIG_JUCE_BUILD_DIR/$FILE" ] && FILE="$ORIG_JUCE_BUILD_DIR/$FILE"
+[ -f "$NEW_JUCE_BUILD_DIR/$FILE"  ] && FILE="$NEW_JUCE_BUILD_DIR/$FILE"
+[ -f "$MY_JUCE_BUILD_DIR/$FILE"   ] && FILE="$MY_JUCE_BUILD_DIR/$FILE"
+[ -f "$FILE"                      ] && kate "$FILE" --line $LINE
