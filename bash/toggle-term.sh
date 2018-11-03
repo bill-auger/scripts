583c0077a0265c61dca8a0d333a1371354973285
bill-auger <mr.j.spam.me@gmail.com>
2018-11-03 07:16:52 -0400

    update bash/toggle-term.sh

--- bash/toggle-term.sh
+++ bash/toggle-term.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+[[ "$1" == 'hide' ]] && readonly SHOULD_HIDE=1 || readonly SHOULD_HIDE=0
 readonly DEBUG=false
 readonly TOGGLE_WINDOW_NAME='toggle-term'
 
@@ -7,16 +8,16 @@ readonly TOGGLE_WINDOW_NAME='toggle-term'
 cached_window_pid=$(cat /tmp/toggle_window_pid 2> /dev/null)
 toggle_window_pid=$(xdotool search --name "^$TOGGLE_WINDOW_NAME$")
 
-if   [[ -z "$toggle_window_pid" ]]
+if   [[ -z "$toggle_window_pid"                              ]] && ((! $SHOULD_HIDE))
 then $DEBUG && echo "window does not exist - launching"
      lxterminal --title="$TOGGLE_WINDOW_NAME"
 
-elif [[ "$toggle_window_pid" != "$cached_window_pid" ]]
+elif [[ "$toggle_window_pid" != "$cached_window_pid"         ]] && ((! $SHOULD_HIDE))
 then $DEBUG && echo "window is not visible - mapping"
      xdotool windowmap      $toggle_window_pid
      xdotool windowactivate $toggle_window_pid 2> /dev/null
 
-elif [[ "$toggle_window_pid" == "$(xdotool getactivewindow)" ]]
+elif [[ "$toggle_window_pid" == "$(xdotool getactivewindow)" ]] || ((  $SHOULD_HIDE ))
 then $DEBUG && echo "window is visible - unmapping"
      xdotool windowunmap $toggle_window_pid
      toggle_window_pid=''
